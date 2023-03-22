7d911bd51571165807879f32d8bcddadfae16648
httpsgabi <gabriellemaia10@hotmail.com.br>
2023-03-22 16:00:33 -0300

    adicionando readme.md

--- TP/trabalho_chat/Cliente.java
+++ TP/trabalho_chat/Cliente.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-<<<<<<< HEAD
  import java.io.BufferedReader;
  import java.io.IOException;
  import java.io.InputStreamReader;
@@ -90,49 +89,3 @@
          done = true;
      }
  }
-=======
- import java.io.*;
- import java.net.*;
- import java.util.Scanner;
- 
- /**
-  *
-  * @author Virginia
-  */
- public class Cliente {
-    public static void main(String[] args) 
-          throws UnknownHostException, IOException {
-      // dispara cliente
-      new Cliente("10.0.0.53", 12345).executa();
-    }
-    
-    private String host;
-    private int porta;
-    
-    public Cliente (String host, int porta) {
-      this.host = host;
-      this.porta = porta;
-    }
-    
-    public void executa() throws UnknownHostException, IOException {
-      Socket cliente = new Socket(this.host, this.porta);
-      System.out.println("O cliente se conectou ao servidor!");
-  
-      // thread para receber mensagens do servidor
-      Recebedor r = new Recebedor(cliente.getInputStream());
-      new Thread(r).start();
-      
-      // lê msgs do teclado e manda pro servidor
-      Scanner teclado = new Scanner(System.in);
-      PrintStream saida = new PrintStream(cliente.getOutputStream());
-      while (teclado.hasNextLine()) {
-        saida.println(teclado.nextLine());
-      }
-      
-      saida.close();
-      teclado.close();
-      cliente.close();    
-    }
-  }
- 
->>>>>>> 8b793c92e806b25c1c05c2896c09ac644e226814
---
+++ TP/trabalho_chat/Readme.md
@@ -0,0 +1,14 @@
+<h1 align="center">Trabalho de TP</h1>
+<h2 align="center">Criação de um chat utilizando Socket e Multithreading </h2>
+
+<h2>Integrantes:</h2>
+
+<p>Guilherme Luiz Gonzaga<p>
+<p>Gabrielle Maia de Andrade Souza</p>
+
+<h3>Dia 22/mar: Preparação o Trabalho</h3>
+<p>Em sala, compilamos o código disponível pela professora Virgínia com 4 arquivos, porém observamos a falta de conexão com mais de 1 chat ao mesmo tempo e também encontramos erros referentes as mensagens transmitidas(como repetições)</p>
+
+<p>Então procuramos e estudamos formas de resolvermos esses problemas, encontramos um código pronto disponível na prataforma <a href"https://cursos.alura.com.br/forum/topico-chat-socket-multithread-36817">Alura</a>, que faz a conexão multichat</p>
+
+<h4 align="center">COLTEC - Colégio Técnico da UFMG</h4>
